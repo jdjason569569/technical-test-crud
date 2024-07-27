860fff5bf3f3a35babf1b2a4f5875c0c536f846e
DESKTOP-B52VILD\david <jeison@goldenrace.com>
2024-07-27 10:17:43 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PostComponent } from './components/post/post.component';
+import { PhotoComponent } from './components/photo/photo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'posts', component: PostComponent },
+  { path: 'photos', component: PhotoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
